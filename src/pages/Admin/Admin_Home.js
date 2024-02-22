78cc0c1422adafcf110b361053c7c9e865c10b13
ShubhamLokhande2147 <shubhamlokhande2147@gmail.com>
2024-02-22 11:34:41 +0530

    update

--- src/pages/Admin/Admin_Home.js
+++ src/pages/Admin/Admin_Home.js
@@ -21,8 +21,8 @@ export default function Admin_Home() {
          <Navbar.Collapse id="basic-navbar-nav">
            <Nav className="me-auto">
              <Nav.Link href="am_table">Attendance_Masters</Nav.Link>
-             <Nav.Link href="#link">Duties</Nav.Link>
-             <Nav.Link href="#link">Emergency_Meet</Nav.Link>
+             {/* <Nav.Link href="#link">Duties</Nav.Link>
+             <Nav.Link href="#link">Emergency_Meet</Nav.Link> */}
            </Nav>
          </Navbar.Collapse>
        </Container>
